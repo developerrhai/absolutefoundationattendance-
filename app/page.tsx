f48175751176ead01cd2f9f0a7d3649798447ec5
Vaibhav Kandharkar <vaibhavk0099#gmail.com>
2026-05-26 11:27:45 +0530

    chnages

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
-export default function Home() {
-  redirect("/attendance");
+// export default function Home() {
+//   redirect("/attendance");
+// }
+export default function AttendancePage() {
+  return (
+    <div>
+      <h1>Attendance Working</h1>
+    </div>
+  );
 }
